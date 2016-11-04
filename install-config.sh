4c7e8ee63ae9888bcc3bccbd051901000c7cd376
Patrick Callier <pcallier@users.noreply.github.com>
2016-11-04 12:04:39 -0700

    Update install-config.sh

--- install-config.sh
+++ install-config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# wget -O - https://raw.githubusercontent.com/pcallier/config_dump/master/install-config.sh | bash 
+
+set -v
 
 git clone git@github.com:pcallier/config_dump ~
 cp config_dump/.vimrc ~/.vim/
